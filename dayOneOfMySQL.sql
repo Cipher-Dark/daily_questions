ccc292de7544c3471def68f66929ccc1803f1b5f
Sagar Bisht <iamsofficials@gmail.com>
2024-09-21 00:03:57 +0530

    MySQL query day 1

---
+++ dayOneOfMySQL.sql
@@ -0,0 +1,3 @@
+create database books_shop_db;
+
+create table books (book_id varchar, title varchar, author_first_name varchar, author_last_name varchar, release_year date, stock_quantity int, pages int);
